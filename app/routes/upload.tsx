b2d8abdfbd27ee772e7c0776bd513c7a77eaefdf
hschuler13 <hschul04@gmail.com>
2026-02-18 16:50:31 -0600

    Added resume feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -61,6 +61,8 @@ function Upload(props) {
     data.feedback = JSON.parse(feedbackText);
     await kv.set(`resume:${ uuid }`, JSON.stringify(data));
     setStatusText('Resume processing complete! Redirecting...');
+
+    navigate(`/resume/${uuid}`);
   }
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
